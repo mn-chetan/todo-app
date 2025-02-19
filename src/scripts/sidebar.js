a9b830215ef476064c15f79e87ff74d1c196d715
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-19 18:45:22 +0530

    Remove imgSrc in createCard function
    - Images looked unprofessional and were not needed

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -1,14 +1,10 @@
 import "../styles/sidebar.css";
-import projectImg from "../img/xenia-radchenko-ezEn4jYrVYQ-unsplash.jpg";
-import taskImg from "../img/erik-mclean-QzpgqElvSiA-unsplash.jpg";
-import completedImg from "../img/frank-okay-R1J6Z1cnJZc-unsplash.jpg";
-import dueSoonImg from "../img/georgie-cobbs-SU35VU5de1o-unsplash.jpg";
 
 function displaySidebar() {
-  const project = createCard("Project", projectImg);
-  const task = createCard("Task", taskImg);
-  const completed = createCard("Completed", completedImg);
-  const dueSoon = createCard("Due Soon", dueSoonImg);
+  const project = createCard("Project");
+  const task = createCard("Task");
+  const completed = createCard("Completed");
+  const dueSoon = createCard("Due Soon");
 
   const sidebar = document.createElement("aside");
   sidebar.id = "sidebar";
@@ -21,7 +17,7 @@ function displaySidebar() {
   document.body.appendChild(sidebar);
 }
 
-function createCard(cardName, imgSrc = null) {
+function createCard(cardName) {
   const div = document.createElement("div");
   div.classList.add("card");
 
@@ -29,12 +25,6 @@ function createCard(cardName, imgSrc = null) {
   h2.textContent = cardName;
 
   div.appendChild(h2);
-
-  if (imgSrc) {
-    const img = document.createElement("img");
-    img.src = imgSrc;
-    div.appendChild(img);
-  }
 
   return { div };
 }
