2ffc40b306dbbb43b8e2f9769fcad3b249e01a54
Chetan M N <165123379+mn-chetan@users.noreply.github.com>
2025-02-19 13:35:03 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devtool: "eval-source-map",
+};
