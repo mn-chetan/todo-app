bbac04c57805163f2fb0866d7710030639bf3834
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-19 16:23:59 +0530

    Add support for JPG images in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.jpg$/i,
+        type: "asset/resource",
+      },
     ],
   },
   devtool: "eval-source-map",
