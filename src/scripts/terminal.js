f83aeae2f0c048169e655fb170f52e9a8adfad66
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-21 09:37:16 +0530

    Add a terminal interface to webpage
    - Heading of 'Terminal v0.0.1'
    - Include 3 window control buttons for visual effect
    - Create button class to handle button creation
    - Button class contains method to return buttons of different color
    - displayTerminal() appends terminal to body

---
+++ src/scripts/terminal.js
@@ -0,0 +1,43 @@
+import "../styles/terminal.css";
+
+function displayTerminal() {
+  const h3 = document.createElement("h3");
+  h3.textContent = "Terminal v0.0.1";
+
+  const windowControls = document.createElement("div");
+  windowControls.id = "window-control";
+
+  const redButton = new Button("red").createButton();
+  const yellowButton = new Button("yellow").createButton();
+  const greenButton = new Button("green").createButton();
+
+  windowControls.appendChild(redButton);
+  windowControls.appendChild(yellowButton);
+  windowControls.appendChild(greenButton);
+
+  const terminalHeader = document.createElement("div");
+  terminalHeader.id = "terminal-header";
+  terminalHeader.appendChild(h3);
+  terminalHeader.appendChild(windowControls);
+
+  const terminal = document.createElement("div");
+  terminal.id = "terminal";
+  terminal.appendChild(terminalHeader);
+
+  document.body.appendChild(terminal);
+}
+
+class Button {
+  constructor(buttonColor) {
+    this.buttonColor = buttonColor;
+  }
+
+  createButton() {
+    const button = document.createElement("button");
+    button.classList.add("window-button");
+    button.id = `${this.buttonColor}-button`;
+    return button;
+  }
+}
+
+export { displayTerminal };
